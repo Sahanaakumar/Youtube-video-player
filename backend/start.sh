6773bba65d56f3012870f1f694b3a0cc9dca9520
Sahanaa <sahanaakumar01@gmail.com>
2026-06-04 19:51:32 +0530

    fix pythonpath for render

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 export PYTHONPATH=/opt/render/project/src/backend/src
-cd src
-exec uvicorn main:app --host 0.0.0.0 --port $PORT
+exec uvicorn src.main:app --host 0.0.0.0 --port $PORT
